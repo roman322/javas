a6d0cfbd1099bed2bbd2b3c7cddb44c0a6cf1721
Roman Mochurad <r.mochurad@gmail.com>
2018-02-25 17:12:05 +0200

    lab3

---
+++ lab3/Batut.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package party;
+
+/**
+ *
+ * @author rmoch
+ */
+public class Batut extends Party{
+    
+
+
+	private double diametr ;
+	
+public Batut () {
+		
+	}
+	
+	public Batut(PartyType partyLocation, PartyLocation name, int price , String material, double hours, String manfucturer, int amount,double diametr) {
+		
+		super(partyLocation, name, price, material, hours, manfucturer,  amount);
+		
+		this.diametr = diametr;
+	 }
+
+   
+
+	public double getdiametr() {
+		return diametr;
+	}
+
+	public void setdiametr(double diametr) {
+		this.diametr = diametr;
+	}
+
+}
+
+
