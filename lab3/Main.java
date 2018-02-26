9af03f1e8f22be1b795bb2e76ee41315367e5bfe
Roman Mochurad <r.mochurad@gmail.com>
2018-02-26 13:31:45 +0200

    lab3 update

--- lab3/Main.java
+++ lab3/Main.java
@@ -5,41 +5,44 @@
  */
 package party;
 
+import java.util.List;
+
 /**
  *
  * @author rmoch
  */
 public class Main {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		HolidayEvent holidayevent = new HolidayEvent("Boston", 20);
+        HolidayEvent holidayEvent = new HolidayEvent("Boston", 20);
 
-		Party Workshop = new Workshop(PartyType.WORKSHOP, PartyLocation.INSIDE, 50, "Event", 2, "Stepana banderu", 40, 15);
+        Party Workshop = new Workshop(PartyType.WORKSHOP, PartyLocation.INSIDE, 50, "Event", 2, "Stepana banderu", 40, 15);
 
-		Party Animator = new Animator(PartyType.ANIMATOR, PartyLocation.INSIDE, 150, "Event", 2, "Geroiv UPA", 26, 25.5);
+        Party Animator = new Animator(PartyType.ANIMATOR, PartyLocation.INSIDE, 150, "Event", 2, "Geroiv UPA", 26, 25.5);
 
-		Party SoapBubbles = new SoapBubble(PartyType.SOAPBUBBLE, PartyLocation.OUTSIDE, 70, "Event", 2.5, "Suxiv", 15,8.6);
+        Party SoapBubbles = new SoapBubble(PartyType.SOAPBUBBLE, PartyLocation.OUTSIDE, 70, "Event", 2.5, "Suxiv", 15, 8.6);
 
-		Party Batut = new Batut(PartyType.BATUT, PartyLocation.OUTSIDE, 90, "Event", 2, "prospect Shevchenka", 13, 12.5);
+        Party Batut = new Batut(PartyType.BATUT, PartyLocation.OUTSIDE, 90, "Event", 2, "prospect Shevchenka", 13, 12.5);
 
-		holidayevent.addParty(Workshop);  
-		holidayevent.addParty(Animator);
-		holidayevent.addParty(SoapBubbles);
-		holidayevent.addParty(Batut);
+        holidayEvent.addParty(Workshop);
+        holidayEvent.addParty(Animator);
+        holidayEvent.addParty(SoapBubbles);
+        holidayEvent.addParty(Batut);
 
-		holidayevent.sortPartyByPrice(holidayevent.getPartyList());
+        holidayEvent.sortPartyByPrice(holidayEvent.getPartyList());
 
-		for (Party party : holidayevent.getPartyList()) {  
+        for (Party party : holidayEvent.getPartyList()) {
 
-			System.out.println(party.getorg_name() + " is cost  " + party.getPrice() + " dollars and duration is "  + party.getHours() + " hours " + party.getType() + " " + party.getaddress() + " " 
-			
-					
-					+ " " + party.getAmount()) ;
+            System.out.println(party.getorg_name() + " is cost  " + party.getPrice() + " dollars and duration is " 
+                    + party.getHours() + " hours " + party.getType() + " " + party.getaddress() + " "
+                    + " " + party.getAmount());
+        }
+        List<Party> resultList = holidayEvent.searchByPartyParameters("OUTSIDE");
+        System.out.println("Find by Location");
+        System.out.println(resultList);
+    }
 
-		}
-
-	}
+  
 
 }
-
