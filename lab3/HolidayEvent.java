ef1508fd0b885ca1c43d5e4298b7c92420cd22f3
Roman Mochurad <r.mochurad@gmail.com>
2018-02-26 13:34:10 +0200

    lab 3 update holidayevent

--- lab3/HolidayEvent.java
+++ lab3/HolidayEvent.java
@@ -5,79 +5,67 @@
  */
 package party;
 
-
-
 import java.util.*;
-import java.util.Comparator;
 
 public class HolidayEvent {
-	private String name = "no name";
-	private int amountOfEvents;
-	private List <Party> partyList;
-	
-	
-	public HolidayEvent(String name, int amounOfEvents) {
 
+    private String name = "no name";
+    private int amountOfEvents;
+    private List<Party> partyList = new LinkedList();
+    List<Party> parameterPartyList = new LinkedList();
+
+    public HolidayEvent(String name, int amounOfEvents) {
         this.name = name;
-
-        this.amountOfEvents = amountOfEvents;
-
         this.partyList = new LinkedList<>();
 
     }
 
-    
-
-
-
     public void addParty(Party addParty) {
-
-        for (Party party : partyList) {
-
-           if (party.getName().equals(addParty.getName())) {
-
-                party.setPrice(party.getPrice() + addParty.getPrice());
-
-                return;
-
-            }
-
-        }
 
         partyList.add(addParty);
 
     }
-    
+
     //sort by price
-     public List<Party> sortPartyByPrice(List<Party> list) {
-    	 list.sort((Party o1, Party o2)->o1.getPrice()-o2.getPrice());
-    	 return list;
+    public List<Party> sortPartyByPrice(List<Party> list) {
+        list.sort((Party o1, Party o2) -> o1.getPrice() - o2.getPrice());
+        return list;
 
-     }
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getamountOfEvents() {
-		return amountOfEvents;
-	}
-	public void setamountOfEvents(int amountOfEvents) {
-		this.amountOfEvents = amountOfEvents;
-	}
-	 public List<Party> getPartyList() {
-
-	    return partyList;
-
-	}
-	  public void setPartyList (List<Party> partyList) {
-
-	    this.partyList = partyList;
     }
 
-    
-	
-}
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getamountOfEvents() {
+        return amountOfEvents;
+    }
+
+    public void setamountOfEvents(int amountOfEvents) {
+        this.amountOfEvents = amountOfEvents;
+    }
+
+    public List<Party> getPartyList() {
+        return partyList;
+
+    }
+
+    public void setPartyList(List<Party> partyList) {
+        this.partyList = partyList;
+    }
+
+    public List<Party> searchByPartyParameters(String param) {
+        List<Party> finalList = new LinkedList<>();
+        for (Party party : partyList) {
+            if (party.getPartyParameter().equalsIgnoreCase(param)) {
+                finalList.add(party);
+            }
+        }
+        return finalList;
+
+    }
+}
