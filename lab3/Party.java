a375bab8b5c933bf8f54e640f56dc9af48934cfe
Roman Mochurad <r.mochurad@gmail.com>
2018-02-26 13:37:40 +0200

    lab 3 update main

--- lab3/Party.java
+++ lab3/Party.java
@@ -9,126 +9,110 @@ package party;
  *
  * @author rmoch
  */
-
-
-
 public class Party {
 
-   
+    private PartyType Type;
+    private PartyLocation partyLocation;
+    private int price;
+    private String partyParameter="";
+    private String org_name;
+    private double hours;
+    private String address;
+    private int amount;
 
-	
-	
-	private PartyType Type;
+    public Party() {
 
-	private PartyLocation partyLocation;
-	private int price;
+    }
 
-	private String org_name;
+    public Party(PartyType Type, PartyLocation partyLocation, int price, String org_name, double hours, String address, int amount) {
+        this.Type = Type;
+        this.partyLocation = partyLocation;
+        this.price = price;
+        this.org_name = org_name;
+        this.hours = hours;
+        this.address = address;
+        this.amount = amount;
+    }
 
-	private double hours;
-	private String address;
+    public Party(PartyLocation partyLocation, int price) {
+        this.partyLocation = partyLocation;
+        this.price = price;
+    }
 
-	
+    public PartyLocation getName() {
 
-	private int amount;
+        return partyLocation;
 
-	
-	
-	 public Party() {
-	      
-	    }
+    }
 
+    public void setName(PartyLocation name) {
 
+        this.partyLocation = name;
 
-	    public Party(PartyType Type, PartyLocation partyLocation, int price, String org_name, double hours, String address, int amount)
- {
-	     
-	    	
-	    
- this.Type = Type;	
-	   
-  this.partyLocation = partyLocation;
-	     
-this.price = price;
-	    
- this.org_name = org_name;
-	 
-    this.hours = hours;
-	  
-   this.address = address;
-	    
- 
-	
-     this.amount = amount;
-	   
-  
-	    }
-	    
-	
-    public Party (PartyLocation partyLocation, int price) {
-	    	this.partyLocation = partyLocation;
-	    	this.price = price;
-	    }
-	    
-	    
-	   		    	
-	    public PartyLocation getName() {
+    }
 
-	        return partyLocation;
+    public void setType(PartyType Type) {
+        this.Type = Type;
+    }
 
-	    }
+    public PartyType getType() {
+        return this.Type;
+    }
 
+    public int getPrice() {
+        return price;
+    }
 
+    public void setPrice(int price) {
+        this.price = price;
+    }
 
-	    public void setName(PartyLocation name) {
+    public String getorg_name() {
+        return org_name;
+    }
 
-	        this.partyLocation = name;
+    public void setOrgname(String org_name) {
+        this.org_name = org_name;
+    }
 
-	    }
-	    
-	    public void setType(PartyType Type) {
-	    	this.Type = Type;
-	    }
-	    
-	    public PartyType getType() {
-	    	return this.Type;
-	    }
-	    
-	public int getPrice() {
-		return price;
-	}
-	public void setPrice(int price) {
-		this.price = price;
-	}
-	public String getorg_name() {
-		return org_name;
-	}
-	public void setorg_name(String org_name) {
-		this.org_name = org_name;
-	}
-	public double getHours() {
-		return hours;
-	}
-	public void sethours(double hours) {
-		this.hours = hours;
-	}
-	public String getaddress() {
-		return address;
-	}
-	public void setaddress(String address) {
-		this.address = address;
-	}
-	
-	public int getAmount() {
-		return amount;
-	}
-	public void setAmount(int amount) {
-		this.amount = amount;
-	}
-	
-  
-   
+    public double getHours() {
+        return hours;
+    }
+
+    public void sethours(double hours) {
+        this.hours = hours;
+    }
+
+    public String getaddress() {
+        return address;
+    }
+
+    public void setaddress(String address) {
+        this.address = address;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
     
+    public PartyLocation getPartyLocation(){
+        return partyLocation;
+    }
+    
+    public void setPartyLocation(PartyLocation partyLocation){
+        this.partyLocation = partyLocation;
+    }
+    
+    public String getPartyParameter(){
+        return partyParameter;
+    }
+    
+    public void setPartyParameter(String partyParameter) {
+        this.partyParameter = partyParameter;
+    }
+
 }
-
-
